cfce2f50a552c231278012d884fd2edc83a0ce6b
n3op2 <paulius.michelevicius@gmail.com>
2018-10-20 08:48:34 +0100

    Particles pt2.

--- public/js/Particle.js
+++ public/js/Particle.js
@@ -1,20 +1,91 @@
-const getRnd = () => Math.random() * (4 - 0.5) + 0.5;
+const getRnd = (min, max) => Math.random() * (max - min) + min;
+const toNeg = (n) => -Math.abs(n); 
+const toPos = (n) => Math.abs(n); 
+const getDist = (t_x, x, t_y, y) => {
+  let dx = t_x - x;
+  let dy = t_y - y;
+  return { dx: dx, dy: dy, dist: Math.sqrt((dx * dx) + (dy * dy)) };
+}
 
 class Particle {
   constructor(x, y, ctx) {
-    this.x = x;
-    this.vx = getRnd();
-    this.y = y;
+    this.ax = x, this.ay = y;
+    this.x = x, this.y = y;
+    this.dx = 0, this.dy = 0;
+    this.direction = 0;
+    this.vel = 2;
+    this.maxV = 5;
     this.ctx = ctx
-    this.r = 10;
+    this.r = getRnd(4, 1);
+    this.f = 1.000873;
+    this.a = 0;
+    this.dist = 0;
+    this.radius = getRnd(1, -1);
+    this.ratio = 0;
+    this.rRnd = getRnd(120, 50);
+    this.aStep = 2 * Math.PI / getRnd(660, 40), this.aStepMax = 0.3;
+    this.col = [Math.ceil(getRnd(155, 10)), Math.ceil(getRnd(150, 0)), 20];
+    this.n = 1;
+    this.moves = {
+      toText: false
+    }
+
     this.draw = () => {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-      ctx.fillStyle = 'green';
+      ctx.fillStyle = 'rgb(' + this.col.join(',') + ')';
       ctx.fill();
     }
+
+    this.restore = (x, y) => {
+      // reset all moves
+      this.moves.toText = false;
+      this.vel = 2;
+      this.radius = getRnd(2, -2);
+      this.aStep = 2 * Math.PI / getRnd(560, 40);
+    }
+
+    this.sendTo = (x, y) => {
+      this.targetX = x;
+      this.targetY = y;
+      this.moves.toText = true;
+    }
+    
+    this.follow = (mx, my, r) => {
+      let { dx, dy, dist } = getDist(mx, this.x, my, this.y);
+      let ratio;
+      if((mx <= this.x + r && mx >= this.x - r) || (my <= this.y + r && my >= this.y - r)) { 
+        if(dist > 1 && !this.moves.toText) {
+          ratio = 1 / dist;
+          this.x = (ratio * dx) + this.x;
+          this.y = (ratio * dy) + this.y;
+        }
+      }
+    }
+
     this.step = () => {
-      this.x += this.vx;
+      if(this.moves.toText){
+        this.dx = this.targetX - this.x;  
+        this.dy = this.targetY - this.y;  
+        this.dist = Math.sqrt((this.dx * this.dx) + (this.dy * this.dy));
+        if(this.dist > this.vel){
+          if(this.aStep < 0.5 && this.aStep > 0.01) this.aStep /= this.f
+          this.vel *= this.f + 0.006;
+          this.ratio = this.vel / this.dist;
+          this.x = (this.ratio * this.dx) + this.x - (this.radius * Math.sin(this.a));
+          this.y = (this.ratio * this.dy) + this.y - (this.radius * Math.cos(this.a));
+        } 
+      } else {
+        if(this.aStep < this.aStepMax) this.aStep *= this.f;
+        if(this.radius > -3 && this.radius < 3) { 
+          this.radius *= this.f;
+        } else {
+          this.radius /= this.f;
+        }
+        this.x += this.radius * Math.sin(this.a);
+        this.y += this.radius * Math.cos(this.a);
+      }
+      this.a += this.aStep;
     }
   }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -3,29 +3,92 @@ const ctx = c.getContext('2d');
 
 const stage_w = c.width;
 const stage_h = c.height;
+let parts = [];
+let mouse_x = 0;
+let mouse_y = 0;
 
 const rnd_w = () => Math.random() * stage_w;
 const clear = () => ctx.clearRect(0, 0, stage_w, stage_h);
 
-const delPart = (i) => arr.splice(i, 1);
-const addPart = () => new Particle(10, rnd_w(), ctx);
-const rePart = (i) => {
-  arr.splice(i, 1)
-  arr.push(new Particle(10, rnd_w(), ctx));
-};
+const addPart = (x, y) => new Particle(x, y, ctx);
+const delPart = (i) => parts.splice(i, 1);
+const rePart = (i) => parts[i] = addPart(parts[i].ax, parts[i].ay);
 
-let arr = Array.from(Array(100), () => addPart());
+const targetReached = (parts) => {
+  return parts.every(el => (el.x > target_x - 50 && el.x < target_x + 50));
+}
 
+let testB = true;
+
+const rect = c.getBoundingClientRect();
+
+
+const createParts = (x, y, str) => {
+  ctx.font = 'bold 100px sans-serif';
+  ctx.textAlign = 'center';
+  ctx.fillText(str, x, y);
+  const data = ctx.getImageData(0, 0, stage_w, stage_h).data;
+  for(let i = 0; i< stage_w; i += Math.round(stage_w / 400)) {
+    for(let j = 0; j < stage_h; j += Math.round(stage_w / 400)) {
+      if(data[((i + j * stage_w) * 4) + 3] > 150) {
+        parts.push(addPart(i, j));
+      }
+    }
+  }
+}
+
+c.addEventListener('mousemove', (e) => {
+  mouse_x = e.clientX - rect.left;
+  mouse_y = e.clientY - rect.top;
+}) 
+c.addEventListener('click', (e) => {
+  console.log('click');
+  testB = !testB;
+  return testB ? 
+    parts.forEach(el => el.restore(el.ax, el.ay)) : 
+    parts.forEach(el => el.sendTo(el.ax, el.ay));
+});
+
+const removeLost = (parts) => {
+  parts.map((el, i) => {
+    if(el.x > stage_w * 2 || el.x < -600) {
+      delPart(i);
+    } else if(el.y > stage_h * 2 || el.y < -600) {
+      delPart(i);
+    }
+  });
+}
+
+const _mC = (n) => Math.ceil(n);
+const inRadius = (el, r) => {
+    return (_mC(el.y) >= (_mC(el.ay)-r) && _mC(el.y) <= (_mC(el.ay)+r)) && 
+    (_mC(el.x) >= (_mC(el.ax)-r) && _mC(el.x) <= (_mC(el.ax)+r));
+   
+}
 const init = () => {
   clear();
-  arr.forEach((el, i) => {
-    if(el.x >= stage_w) {
-      rePart(i);
+  parts.forEach(el => el.follow(mouse_x, mouse_y, 20));
+  parts.forEach((el, i) => {
+    if(mouse_x <= el.x + 20 && mouse_x >= el.x - 20) {
+      if(el.radius < 5) {
+        el.radius *= 1.0092;
+      }
     }
+  if(inRadius(el, 50)) {
+    if(el.col[0] <= 255) el.col[0] += 20;
+    if(el.col[1] >= 0) el.col[1] -= 20;;
+  } else {
+    if(el.col[0] >= 0) el.col[0] -= 5;
+    if(el.col[1] <= 255) el.col[1] += 5;
+  }
+  if(i % 40 === 0 && el.moves.toText) {
+    el.moves.toText = false;
+  }
     return (el.draw(), el.step())
   });
-  console.log(arr.length);
+//  removeLost(parts);
   window.requestAnimationFrame(init);   
 }
 
+createParts(100, 300, 'yo');
 init();
